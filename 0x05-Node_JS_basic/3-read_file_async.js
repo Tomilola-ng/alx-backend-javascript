0e2b6e751e3ffa2e8b3844a48019825d80022120
Tomilola-ng <tee.o2809@gmail.com>
2024-08-21 02:25:02 +0100

    SEAT_FOR 30_MINUTES

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,60 @@
+/**
+ * Retrieves the count of students from a CSV file.
+ * @param {String} dataPath Path to the CSV file, read asynchronously.
+ * @returns {Promise} A promise that resolves with the student count.
+ * @author Tomilola Oluwafemi <https://github.com/Tomilola-ng>
+ */
+
+const fs = require("fs");
+
+const countStudents = (dataPath) =>
+  // Read the file asynchronously and split its content into lines
+  new Promise((resolve, reject) => {
+    fs.readFile(dataPath, "utf-8", (err, data) => {
+      if (err) {
+        reject(new Error("Cannot load the database"));
+      }
+      if (data) {
+        const fileLines = data.toString("utf-8").trim().split("\n");
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(",");
+        const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+        // Process each line to categorize students into groups
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(",");
+          const studentPropValues = studentRecord.slice(
+            0,
+            studentRecord.length - 1
+          );
+          // Associate student properties with their corresponding values
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
+          }
+          const studentEntries = studentPropNames.map((propName, idx) => [
+            propName,
+            studentPropValues[idx],
+          ]);
+          studentGroups[field].push(Object.fromEntries(studentEntries));
+        }
+
+        // Count the total number of students
+        const totalStudents = Object.values(studentGroups).reduce(
+          (pre, cur) => (pre || []).length + cur.length
+        );
+        console.log(`Number of students: ${totalStudents}`);
+        for (const [field, group] of Object.entries(studentGroups)) {
+          const studentNames = group
+            .map((student) => student.firstname)
+            .join(", ");
+          console.log(
+            `Number of students in ${field}: ${group.length}. List: ${studentNames}`
+          );
+        }
+        resolve(true);
+      }
+    });
+  });
+
+module.exports = countStudents;
